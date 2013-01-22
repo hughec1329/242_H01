be5f5377ffebed77a281ffe991522df329309302
hughec1329 <hughcrockford@gmail.com>
2013-01-21 16:09:42 -0800

    added map and color for percent late

--- H01_airline.R
+++ H01_airline.R
@@ -30,13 +30,32 @@ trim = BayAreaDelays[,c(2,6,9,10,15,24,30,31:32,41:43,48:52,55,57:61)]
 # use of airports
 #####################
 
+# which airports are the worst for on time performance?
+
 table(trim$ORIGIN)
 table(trim$DEST)
 trim = transform(trim, inbound =  trim$DEST %in% c("SFO","OAK","SMF"), outbound = trim$ORIGIN %in% c("SFO","OAK","SMF"))	#  inbound vs outbound.
-latelimit = 30
+latelimit = 30                         # 
 trim$isLate = (trim$inbound == TRUE & trim$ARR_DELAY > latelimit) | (trim$outbound == TRUE & trim$DEP_DELAY > latelimit )
-tapply(trim$isLate, trim$DEST , sum,na.rm = TRUE) / tapply(trim$isLate, trim$DEST , length)  
+late = tapply(trim$isLate, trim$DEST , sum,na.rm = TRUE) / tapply(trim$isLate, trim$DEST , length)  
+barplot(late[order(late)])
 
+# by map - color scale to red of lateness??
+library(maps)
+library(maptools)
+library(RColorBrewer)
+ports = readShapeSpatial("./airports/airports.shp")
+map("state")
+air = ports@data$LOCID
+colScale = colorRampPalette(brewer.pal(9,"YlOrRd"))(length(late))
+points(ports[air %in% rownames(late),],pch = 20,col = colScale[rank(late)],cex = 3)
+
+# playt with color ramp
+
+col = colorRamp(colors = c("white","red"))
+points(ports[air %in% rownames(late),],pch = 20,col = t(late),cex = 2)
+
+# size by rank lateness
 
 ###############
 # how late are most of the late planes"
@@ -91,3 +110,15 @@ densityplot(m.datT$FL_DATE, groups=m.datT$CARRIER)
 # delay explained by recent flights - to what number?
 late = as.numeric(del$isLate)
 plot(late[200:500],type = "l")
+
+# whichc airline takes loongest to taxi over same route - pref treatment by air traffic control?
+taxi = BayAreaDelays[,c(6,9,15,24,31,37:40,42)]
+names(taxi)
+
+# arriving flights
+
+trim = transform(trim, inbound =  trim$DEST %in% c("SFO","OAK","SMF"), outbound = trim$ORIGIN %in% c("SFO","OAK","SMF"))	#  inbound vs outbound.
+
+
+# add in weather data - Rcurl fill form to source data?
+## do like thi grant - cumulative days of shit weather, wind above threshold, test diff thresholds.
